e6f0ca87f36ba22e4f3c4bfaa73aef1c97d59328
klasing <klasing1959@gmail.com>
2017-11-23 19:02:56 +0100

    23-11-2017 19:02 T12-01 with one activity: ColorFontActivity

---
+++ VisualPolish/app/src/main/java/com/example/visualpolish/ColorFontActivity.java
@@ -0,0 +1,13 @@
+package com.example.visualpolish;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ColorFontActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_font_activity);
+
+    }
+}
--- VisualPolish/app/src/main/java/com/example/visualpolish/MainActivity.java
+++ VisualPolish/app/src/main/java/com/example/visualpolish/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.visualpolish;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView colorAndFont = (TextView) findViewById(R.id.colorAndFont);
+
+        assert colorAndFont != null;
+        colorAndFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numbersIntent = new Intent(MainActivity.this, ColorFontActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
     }
 }
